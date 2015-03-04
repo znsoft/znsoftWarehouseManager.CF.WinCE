875d26d433cde8b71a0b5d1773709eb724594ce9
ValentinLoshkarev <valentin.loshkarev@gmail.com>
2015-03-04 13:24:32 +1000

    1.6.3
    Добавил передачу на ТСД информацию о ошибках при приеме грузовых мест.

--- СкладУчет/02. Общие модули/Program.cs
+++ СкладУчет/02. Общие модули/Program.cs
@@ -13,7 +13,7 @@ namespace СкладскойУчет
         [MTAThread]
         static void Main(string[] args)
         {
-            СоединениеВебСервис.НомерВерсии = "c#1.6.2";
+            СоединениеВебСервис.НомерВерсии = "c#1.6.3";
             Инфо.ИмяЭтогоФайла = Assembly.GetCallingAssembly().ManifestModule.FullyQualifiedName;
             Инфо.АргументЗапуска = null;
 
