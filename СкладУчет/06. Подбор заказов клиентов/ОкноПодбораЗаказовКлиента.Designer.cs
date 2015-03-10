d6116df74dad56d97204dd7eec6e03a1a25b89b7
AlexMiller55 <alex55900@yandex.ru>
2015-03-10 14:41:33 +1000

    1.6.5
    Мелкие правки подбора. Переобозвал папки.

--- СкладУчет/06. Подбор заказов клиентов/ОкноПодбораЗаказовКлиента.Designer.cs
+++ СкладУчет/06. Подбор заказов клиентов/ОкноПодбораЗаказовКлиента.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace СкладскойУчет
 {
-    partial class ОкноПодбораТоваров
+    partial class ОкноПодбораЗаказовКлиента
     {
         /// <summary>
         /// Требуется переменная конструктора.
@@ -29,35 +29,23 @@
         private void InitializeComponent()
         {
             this.НадписьАдрес = new System.Windows.Forms.Label();
-            this.НадписьДинамическаяЯчейка = new System.Windows.Forms.Label();
             this.ДопИнфо = new System.Windows.Forms.Label();
-            this.КнопкаПовторитьЗавершение = new System.Windows.Forms.Button();
             this.СписокВыбора = new System.Windows.Forms.ListView();
             this.Товар = new System.Windows.Forms.ColumnHeader();
             this.Количество = new System.Windows.Forms.ColumnHeader();
-            this.МенюПодбора = new System.Windows.Forms.ContextMenu();
-            this.Вычерк = new System.Windows.Forms.MenuItem();
-            this.Завершить = new System.Windows.Forms.MenuItem();
-            this.Меню = new System.Windows.Forms.Button();
             this.Гуид = new System.Windows.Forms.ColumnHeader();
+            this.НадписьРН = new System.Windows.Forms.Label();
+            this.Далее = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // НадписьАдрес
             // 
             this.НадписьАдрес.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
-            this.НадписьАдрес.Location = new System.Drawing.Point(69, 1);
+            this.НадписьАдрес.Location = new System.Drawing.Point(64, 0);
             this.НадписьАдрес.Name = "НадписьАдрес";
-            this.НадписьАдрес.Size = new System.Drawing.Size(168, 19);
+            this.НадписьАдрес.Size = new System.Drawing.Size(113, 19);
             this.НадписьАдрес.Text = "Адрес: А01-01-1";
-            // 
-            // НадписьДинамическаяЯчейка
-            // 
-            this.НадписьДинамическаяЯчейка.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
-            this.НадписьДинамическаяЯчейка.Location = new System.Drawing.Point(0, 300);
-            this.НадписьДинамическаяЯчейка.Name = "НадписьДинамическаяЯчейка";
-            this.НадписьДинамическаяЯчейка.Size = new System.Drawing.Size(240, 19);
-            this.НадписьДинамическаяЯчейка.Text = "Сосканируйте ДЯ";
-            this.НадписьДинамическаяЯчейка.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.НадписьАдрес.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // ДопИнфо
             // 
@@ -67,29 +55,17 @@
             this.ДопИнфо.Size = new System.Drawing.Size(240, 53);
             this.ДопИнфо.Text = "Дополнительная информация по товару";
             // 
-            // КнопкаПовторитьЗавершение
-            // 
-            this.КнопкаПовторитьЗавершение.BackColor = System.Drawing.Color.LightSalmon;
-            this.КнопкаПовторитьЗавершение.Location = new System.Drawing.Point(0, 244);
-            this.КнопкаПовторитьЗавершение.Name = "КнопкаПовторитьЗавершение";
-            this.КнопкаПовторитьЗавершение.Size = new System.Drawing.Size(240, 53);
-            this.КнопкаПовторитьЗавершение.TabIndex = 5;
-            this.КнопкаПовторитьЗавершение.Text = "Повторить завершение задания";
-            this.КнопкаПовторитьЗавершение.Visible = false;
-            this.КнопкаПовторитьЗавершение.Click += new System.EventHandler(this.КнопкаПовторитьЗавершение_Click);
-            // 
             // СписокВыбора
             // 
             this.СписокВыбора.Activation = System.Windows.Forms.ItemActivation.TwoClick;
             this.СписокВыбора.Columns.Add(this.Товар);
             this.СписокВыбора.Columns.Add(this.Количество);
             this.СписокВыбора.Columns.Add(this.Гуид);
-            this.СписокВыбора.ContextMenu = this.МенюПодбора;
             this.СписокВыбора.FullRowSelect = true;
             this.СписокВыбора.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.СписокВыбора.Location = new System.Drawing.Point(-1, 18);
+            this.СписокВыбора.Location = new System.Drawing.Point(-1, 19);
             this.СписокВыбора.Name = "СписокВыбора";
-            this.СписокВыбора.Size = new System.Drawing.Size(241, 226);
+            this.СписокВыбора.Size = new System.Drawing.Size(241, 225);
             this.СписокВыбора.TabIndex = 4;
             this.СписокВыбора.Tag = "Таблица";
             this.СписокВыбора.View = System.Windows.Forms.View.Details;
@@ -106,55 +82,46 @@
             this.Количество.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.Количество.Width = 80;
             // 
-            // МенюПодбора
-            // 
-            this.МенюПодбора.MenuItems.Add(this.Вычерк);
-            this.МенюПодбора.MenuItems.Add(this.Завершить);
-            // 
-            // Вычерк
-            // 
-            this.Вычерк.Text = "Вычерк";
-            this.Вычерк.Click += new System.EventHandler(this.Вычерк_Click);
-            // 
-            // Завершить
-            // 
-            this.Завершить.Text = "Завершить подбор";
-            this.Завершить.Click += new System.EventHandler(this.Завершить_Click);
-            // 
-            // Меню
-            // 
-            this.Меню.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.Меню.BackColor = System.Drawing.SystemColors.Menu;
-            this.Меню.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
-            this.Меню.Location = new System.Drawing.Point(0, -1);
-            this.Меню.Name = "Меню";
-            this.Меню.Size = new System.Drawing.Size(61, 21);
-            this.Меню.TabIndex = 11;
-            this.Меню.Text = "&0.меню";
-            this.Меню.Click += new System.EventHandler(this.Меню_Click);
-            // 
             // Гуид
             // 
             this.Гуид.Text = "ColumnHeader";
             this.Гуид.Width = 0;
             // 
-            // ОкноПодбораТоваров
+            // НадписьРН
+            // 
+            this.НадписьРН.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
+            this.НадписьРН.Location = new System.Drawing.Point(0, 299);
+            this.НадписьРН.Name = "НадписьРН";
+            this.НадписьРН.Size = new System.Drawing.Size(113, 19);
+            this.НадписьРН.Text = "РН: Аэ3-123456";
+            this.НадписьРН.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Далее
+            // 
+            this.Далее.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.Далее.Location = new System.Drawing.Point(138, 298);
+            this.Далее.Name = "Далее";
+            this.Далее.Size = new System.Drawing.Size(100, 20);
+            this.Далее.TabIndex = 17;
+            this.Далее.Text = "Завершить";
+            this.Далее.Click += new System.EventHandler(this.Далее_Click);
+            // 
+            // ОкноПодбораЗаказовКлиента
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(240, 320);
             this.ControlBox = false;
+            this.Controls.Add(this.Далее);
+            this.Controls.Add(this.НадписьРН);
             this.Controls.Add(this.СписокВыбора);
-            this.Controls.Add(this.Меню);
-            this.Controls.Add(this.НадписьДинамическаяЯчейка);
             this.Controls.Add(this.НадписьАдрес);
-            this.Controls.Add(this.КнопкаПовторитьЗавершение);
             this.Controls.Add(this.ДопИнфо);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.KeyPreview = true;
             this.MinimizeBox = false;
-            this.Name = "ОкноПодбораТоваров";
+            this.Name = "ОкноПодбораЗаказовКлиента";
             this.Text = "Подбор товара";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.ОкноПодбораЗаданий_Load);
@@ -166,17 +133,13 @@
         #endregion
 
         private System.Windows.Forms.Label НадписьАдрес;
-        private System.Windows.Forms.Label НадписьДинамическаяЯчейка;
         private System.Windows.Forms.Label ДопИнфо;
-        private System.Windows.Forms.Button КнопкаПовторитьЗавершение;
         public System.Windows.Forms.ListView СписокВыбора;
         private System.Windows.Forms.ColumnHeader Товар;
         private System.Windows.Forms.ColumnHeader Количество;
-        private System.Windows.Forms.Button Меню;
-        private System.Windows.Forms.ContextMenu МенюПодбора;
-        private System.Windows.Forms.MenuItem Вычерк;
-        private System.Windows.Forms.MenuItem Завершить;
         private System.Windows.Forms.ColumnHeader Гуид;
+        private System.Windows.Forms.Label НадписьРН;
+        private System.Windows.Forms.Button Далее;
 
     }
 }
