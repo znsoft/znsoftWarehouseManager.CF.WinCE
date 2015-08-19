41a1595ebd4dbc66c96407099dbdd1fc85ca3209
ValentinLoshkarev <valentin.loshkarev@gmail.com>
2015-08-19 17:03:32 +1000

    1.7.8
    Переработан поиск по штрих коду

--- СкладУчет/02. Общие модули/Program.cs
+++ СкладУчет/02. Общие модули/Program.cs
@@ -13,7 +13,7 @@ namespace СкладскойУчет
         [MTAThread]
         static void Main(string[] args)
         {
-            СоединениеВебСервис.НомерВерсии = "c#1.7.7";
+            СоединениеВебСервис.НомерВерсии = "c#1.7.8";
             Инфо.ИмяЭтогоФайла = Assembly.GetCallingAssembly().ManifestModule.FullyQualifiedName;
             Инфо.АргументЗапуска = null;
 
--- СкладУчет/04. Общие формы/ВыборТовараИзМножества.Designer.cs
+++ СкладУчет/04. Общие формы/ВыборТовараИзМножества.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.СписокВыбора = new System.Windows.Forms.ListView();
+            this.Код = new System.Windows.Forms.ColumnHeader();
+            this.Товар = new System.Windows.Forms.ColumnHeader();
+            this.КолонкаГуид = new System.Windows.Forms.ColumnHeader();
             this.Назад = new System.Windows.Forms.Button();
             this.Далее = new System.Windows.Forms.Button();
             this.Инструкция = new System.Windows.Forms.Label();
             this.ДопИнфо = new System.Windows.Forms.Label();
-            this.Код = new System.Windows.Forms.ColumnHeader();
-            this.Товар = new System.Windows.Forms.ColumnHeader();
-            this.КолонкаГуид = new System.Windows.Forms.ColumnHeader();
             this.SuspendLayout();
             // 
             // СписокВыбора
@@ -45,51 +45,14 @@
             this.СписокВыбора.Columns.Add(this.Товар);
             this.СписокВыбора.Columns.Add(this.КолонкаГуид);
             this.СписокВыбора.FullRowSelect = true;
-            this.СписокВыбора.Location = new System.Drawing.Point(5, 30);
+            this.СписокВыбора.Location = new System.Drawing.Point(2, 25);
             this.СписокВыбора.Name = "СписокВыбора";
-            this.СписокВыбора.Size = new System.Drawing.Size(230, 116);
+            this.СписокВыбора.Size = new System.Drawing.Size(236, 116);
             this.СписокВыбора.TabIndex = 0;
             this.СписокВыбора.Tag = "Таблица";
             this.СписокВыбора.View = System.Windows.Forms.View.Details;
             this.СписокВыбора.ItemActivate += new System.EventHandler(this.СписокВыбора_ItemActivate);
             this.СписокВыбора.SelectedIndexChanged += new System.EventHandler(this.СписокВыбора_SelectedIndexChanged);
-            // 
-            // Назад
-            // 
-            this.Назад.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
-            this.Назад.Location = new System.Drawing.Point(5, 283);
-            this.Назад.Name = "Назад";
-            this.Назад.Size = new System.Drawing.Size(100, 25);
-            this.Назад.TabIndex = 1;
-            this.Назад.Text = "&0. Назад";
-            this.Назад.Click += new System.EventHandler(this.Назад_Click);
-            // 
-            // Далее
-            // 
-            this.Далее.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
-            this.Далее.Location = new System.Drawing.Point(135, 283);
-            this.Далее.Name = "Далее";
-            this.Далее.Size = new System.Drawing.Size(100, 25);
-            this.Далее.TabIndex = 2;
-            this.Далее.Text = "&1. Далее";
-            this.Далее.Click += new System.EventHandler(this.Далее_Click);
-            // 
-            // Инструкция
-            // 
-            this.Инструкция.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
-            this.Инструкция.Location = new System.Drawing.Point(6, 7);
-            this.Инструкция.Name = "Инструкция";
-            this.Инструкция.Size = new System.Drawing.Size(228, 19);
-            this.Инструкция.Tag = "Инструкция";
-            this.Инструкция.Text = "Выберите товар";
-            // 
-            // ДопИнфо
-            // 
-            this.ДопИнфо.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular);
-            this.ДопИнфо.Location = new System.Drawing.Point(5, 149);
-            this.ДопИнфо.Name = "ДопИнфо";
-            this.ДопИнфо.Size = new System.Drawing.Size(230, 128);
-            this.ДопИнфо.Text = "Дополнительная информация по товару";
             // 
             // Код
             // 
@@ -105,6 +68,44 @@
             // 
             this.КолонкаГуид.Text = "Гуид";
             this.КолонкаГуид.Width = 0;
+            // 
+            // Назад
+            // 
+            this.Назад.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.Назад.Location = new System.Drawing.Point(2, 296);
+            this.Назад.Name = "Назад";
+            this.Назад.Size = new System.Drawing.Size(105, 22);
+            this.Назад.TabIndex = 1;
+            this.Назад.Text = "&0. Назад";
+            this.Назад.Click += new System.EventHandler(this.Назад_Click);
+            // 
+            // Далее
+            // 
+            this.Далее.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.Далее.Location = new System.Drawing.Point(133, 296);
+            this.Далее.Name = "Далее";
+            this.Далее.Size = new System.Drawing.Size(105, 22);
+            this.Далее.TabIndex = 2;
+            this.Далее.Text = "&1. Далее";
+            this.Далее.Click += new System.EventHandler(this.Далее_Click);
+            // 
+            // Инструкция
+            // 
+            this.Инструкция.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.Инструкция.Location = new System.Drawing.Point(2, 2);
+            this.Инструкция.Name = "Инструкция";
+            this.Инструкция.Size = new System.Drawing.Size(236, 20);
+            this.Инструкция.Tag = "Инструкция";
+            this.Инструкция.Text = "ВЫБЕРИТЕ ТОВАР";
+            this.Инструкция.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ДопИнфо
+            // 
+            this.ДопИнфо.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular);
+            this.ДопИнфо.Location = new System.Drawing.Point(2, 144);
+            this.ДопИнфо.Name = "ДопИнфо";
+            this.ДопИнфо.Size = new System.Drawing.Size(236, 149);
+            this.ДопИнфо.Text = "Дополнительная информация по товару";
             // 
             // ВыборТовараИзМножества
             // 
